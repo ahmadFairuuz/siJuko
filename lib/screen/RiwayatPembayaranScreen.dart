5f36fee37abc97abb518cb8fb5ca485764c02555
ahmadFairuuz <ahmadfruz@gmail.com>
2025-11-20 14:31:20 +0700

    add denda

---
+++ lib/screen/RiwayatPembayaranScreen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import '../api/KegiatanAPI.dart';
+import '../component/KegiatanListItem.dart';
+import '../data_model/KegiatanModel.dart';
+
+class KegiatanScreen extends StatefulWidget {
+  const KegiatanScreen({super.key});
+
+  @override
+  State<KegiatanScreen> createState() => _KegiatanScreenState();
+}
+
+class _KegiatanScreenState extends State<KegiatanScreen> {
+  Future<List<KegiatanModel>> dataKegiatan = Future.value([]);
+  final RefreshController _refreshController = RefreshController(
+    initialRefresh: false,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() async {
+    List<KegiatanModel> data = await KegiatanApi.getKegiatan();
+    setState(() {
+      dataKegiatan = Future.value(data);
+    });
+  }
+
+  void refreshData() async {
+    await Future.delayed(Duration(milliseconds: 1000));
+    getData();
+    _refreshController.refreshCompleted();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Kegiatan Terdekat',
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            color: Colors.black,
+          ), // Menggunakan font Poppins dengan warna hitam
+        ),
+      ),
+      body: FutureBuilder(
+        future: dataKegiatan,
+        builder: (context, AsyncSnapshot<List<KegiatanModel>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.data!.isNotEmpty) {
+              return Container(
+                padding: const EdgeInsets.all(10),
+                width: MediaQuery.of(context).size.width,
+                child: SmartRefresher(
+                  controller: _refreshController,
+                  onRefresh: refreshData,
+                  enablePullUp: true,
+                  child: ListView.separated(
+                    itemBuilder: (context, index) =>
+                        KegiatanItem(dataKegiatan: snapshot.data![index]),
+                    separatorBuilder: (context, index) =>
+                        Container(height: 1, color: Colors.black26),
+                    itemCount: snapshot.data!.length,
+                  ),
+                ),
+              );
+            } else {
+              return const Center(child: Text('Belum ada Kegiatan Terdekat'));
+            }
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
